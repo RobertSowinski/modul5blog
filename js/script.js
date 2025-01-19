76ad48d874d056f2aa1ff623cc51351604b9e205
Robert Sowiński <saironeandonly@gmail.com>
2025-01-19 19:08:58 +0100

    finish blog, handlebars, eslint now works because of /*global Handlebars */ at the start of script.js

--- js/script.js
+++ js/script.js
@@ -1,4 +1,6 @@
 'use strict';
+/* global Handlebars */
+
 const templates = {
   articleLink: Handlebars.compile(document.querySelector('#template-article-link').innerHTML),
   tagLinkInArticle: Handlebars.compile(document.querySelector('#template-tag-link-in-article').innerHTML),
